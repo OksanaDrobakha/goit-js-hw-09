318b9d6544cc64b87061012c0980791d65fc81cc
OksanaDrobakha <6249784@gmail.com>
2023-03-02 23:53:33 +0400

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+btnStop.disabled = true;
+let timeInterval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', event => {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timeInterval = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', event => {
+  clearInterval(timeInterval);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
